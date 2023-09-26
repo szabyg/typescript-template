03a3779126eb343406b30477e777b05b2de838df
Szabolcs Grünwald <sgruenwald@eurofunk.com>
2023-09-26 17:26:03 +0200

    Initial commit

---
+++ src/foo/bar.spec.ts
@@ -0,0 +1,5 @@
+import {sum} from "./bar";
+
+test('bar', () => {
+    expect(sum(1,2)).toBe(3)
+})
---
+++ src/foo/bar.ts
@@ -0,0 +1,3 @@
+export function sum(...a: number[]) {
+  return a.reduce((acc, val) => acc + val, 0);
+}
